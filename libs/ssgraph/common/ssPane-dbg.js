71df17afb92d5b1ffb771f08c2055291c1aac8c1
S0019132848 <kmitchell@illumiti.com>
2019-08-28 22:13:32 +0000

    Import content for version 1.0.154

---
+++ libs/ssgraph/common/ssPane-dbg.js
@@ -0,0 +1,361 @@
+sap.ui.define([
+	"zss5lcabase/common/ssMath",
+	"zss5lcabase/common/coreFunctions",
+	"zss5lgraphics/libs/ssgraph/common/ssBase",
+		"zss5lgraphics/libs/ssgraph/common/ssScreen"
+], function(ssMath,core,ssBase,ssScreen) {
+	return function(myParent,x,y) {
+		this.data = myParent.getData();
+		this.ssparent = myParent;
+		this.client = myParent.client;
+		this.x = x;
+		this.y = y;
+		this.w = 0;
+		this.h = 0;
+		this.isGlass = false;
+		this.isTemperedGlass = false;
+		this.paneo = undefined;
+		this.isReady = false;
+	
+	    if(this.x === undefined){
+			this.x = 0;
+		}
+		if(this.y === undefined){
+			this.y = 0;
+		}	
+		
+		this.setData = function(data) {
+			this.data = data;
+		};
+		this.getData = function() {
+			return this.data;
+		};
+		this.setParent= function(parent) {
+			this.ssparent = parent;
+		};
+		this.getParent = function() {
+			return this.ssparent;
+		};
+		this.getClient = function() {
+			return this.client;
+		};
+		this.render = function() {
+			var data = this.getData();
+			var color = 'WH';
+			var ismosm = "O";
+			var showGlassMarks = true;
+			var showVinylMarks = false;
+			var isHorizontal = data.isHorizontal;
+			if(isHorizontal === undefined){isHorizontal = false;}
+
+	        if(!this.getParent().isReady) return this;			
+
+            var hasSpreader = false;
+             if(data.model["SPREADER.BAR"] !=undefined){
+               if(data.model["SPREADER.BAR"].value){hasSpreader = true;}
+ 	        }
+
+
+ 	        var vtype = "";
+ 	        if(data.model["VENT.TYPE"] !=undefined){
+ 	        	vtype = data.model["VENT.TYPE"].value;
+ 	        }
+
+
+			var ktype = "";
+ 	        if(data.model["KNEEWALL.TYPE"] !=undefined){
+ 	        	ktype = data.model["KNEEWALL.TYPE"].value;
+ 	        	if(ktype === 'SC'){
+ 	        		showGlassMarks = false;
+ 	        	}
+ 	        }
+ 	        if(data.model['ISM.OSM'] != undefined && data.model['ISM.OSM'].value != undefined){
+              ismosm = data.model['ISM.OSM'].value;
+ 	        } 
+
+ 	        if((vtype === "" || vtype==="V") &&  ktype === "" && data.model["VINYL.COLOR"] !=undefined){
+               color = data.model['VINYL.COLOR'].value;
+               showVinylMarks = true;
+               showGlassMarks = false;
+               if(data.isMixedVents){
+               	  switch(data.currentVent){
+               	  	case 0:
+                     color = data.model['VENT1.VINYL.COLOR'].value;
+                     break;
+               	  	case 1:
+                     color = data.model['VENT2.VINYL.COLOR'].value;
+                     break;
+               	  	case 2:
+                     color = data.model['VENT3.VINYL.COLOR'].value;
+                     break;
+               	  	case 3:
+                     color = data.model['VENT4.VINYL.COLOR'].value;
+               	  	 break;
+               	  }
+               }
+ 	        }   
+ 	        else{
+               if(vtype === "" && data.model["GLASS.COLOR"] !=undefined){
+                 color = data.model['GLASS.COLOR'].value; 	        	
+                 this.isGlass = true;
+                 if(data.model["TEMPERED.GLASS"] !=undefined){
+                   this.isTemperedGlass = data.model['TEMPERED.GLASS'].value; 	        	
+                 } 
+               }  
+ 	        }
+
+			var hasScreenOnly = false;
+			if(data.model.hasScreenOnly != undefined){
+				hasScreenOnly = data.model.hasScreenOnly;
+			}
+			if(hasScreenOnly){
+				showGlassMarks = false;
+				showVinylMarks = false;
+            }
+            
+			this.paneo = {container:undefined, svg: undefined};
+			this.paneo.container  = this.getParent().sashi.container.nested();
+            this.paneo.w = this.getParent().sashi.w;
+            this.paneo.h = this.getParent().sashi.h;
+            this.paneo.x = this.x;
+            this.paneo.y = this.y;
+			
+			this.paneo.svg  = this.paneo.container.rect(this.paneo.w, this.paneo.h);
+			this.paneo.svg.attr({fill: ssBase.color[color].htmlcolor,"stroke": "#000", "stroke-width": 1});
+            this.paneo.svg.move(this.paneo.x,this.paneo.y);
+
+            if(showGlassMarks && !hasSpreader){
+              var  centerx = this.x + (this.paneo.w/2);
+              var  centery = this.y + (this.paneo.h/2);
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-15,centerx-15,centery+5);
+			  this.paneo.svgx.attr({fill: ssBase.color["DG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+20,centery-20,centerx-20,centery+20);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+15,centery-5,centerx-5,centery+15);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+            } 
+
+
+            if(!isHorizontal && showGlassMarks && hasSpreader){
+              var  centerx = this.x + (this.paneo.w/4);
+              var  centery = this.y + (this.paneo.h/2);
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-15,centerx-15,centery+5);
+			  this.paneo.svgx.attr({fill: ssBase.color["DG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+20,centery-20,centerx-20,centery+20);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+15,centery-5,centerx-5,centery+15);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+
+              centerx = this.x + ((this.paneo.w/4) * 3);
+              centery = this.y + (this.paneo.h/2);
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-15,centerx-15,centery+5);
+			  this.paneo.svgx.attr({fill: ssBase.color["DG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+20,centery-20,centerx-20,centery+20);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+15,centery-5,centerx-5,centery+15);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+            } 
+            
+
+            if(isHorizontal && showGlassMarks && hasSpreader){
+              var  centerx = this.x + (this.paneo.w/2);
+              var  centery = this.y + (this.paneo.h/4);
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-15,centerx-15,centery+5);
+			  this.paneo.svgx.attr({fill: ssBase.color["DG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+20,centery-20,centerx-20,centery+20);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+15,centery-5,centerx-5,centery+15);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+
+              centerx = this.x + (this.paneo.w/2);
+              centery = this.y + ((this.paneo.h/4) * 3);
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-15,centerx-15,centery+5);
+			  this.paneo.svgx.attr({fill: ssBase.color["DG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+20,centery-20,centerx-20,centery+20);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+			  this.paneo.svgx = this.paneo.container.line(centerx+15,centery-5,centerx-5,centery+15);
+			  this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+            } 
+            
+            if(showVinylMarks && !hasSpreader){
+              var  centerx = this.x + (this.paneo.w/2);
+              var  centery = this.y + (this.paneo.h/2);
+              
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery-5,centerx-5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery-5,centerx,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery,centerx+5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-5,centerx+10,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery,centerx-5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery,centerx,centery+5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery+5,centerx+5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery,centerx+10,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+            } 
+
+            if(isHorizontal && showVinylMarks && hasSpreader){
+              var  centerx = this.x + (this.paneo.w/2);
+              var  centery = this.y + (this.paneo.h/4);
+              
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery-5,centerx-5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery-5,centerx,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery,centerx+5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-5,centerx+10,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery,centerx-5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery,centerx,centery+5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery+5,centerx+5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery,centerx+10,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+
+              centerx = this.x + (this.paneo.w/2);
+              centery = this.y + ((this.paneo.h/4) * 3);
+              
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery-5,centerx-5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery-5,centerx,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery,centerx+5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-5,centerx+10,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery,centerx-5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery,centerx,centery+5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery+5,centerx+5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery,centerx+10,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+           } 
+            if(!isHorizontal && showVinylMarks && hasSpreader){
+              var  centerx = this.x + (this.paneo.w/4);
+              var  centery = this.y + (this.paneo.h/2);
+              
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery-5,centerx-5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery-5,centerx,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery,centerx+5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-5,centerx+10,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery,centerx-5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery,centerx,centery+5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery+5,centerx+5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery,centerx+10,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+
+              centerx = this.x + ((this.paneo.w/4) * 3);
+              centery = this.y + (this.paneo.h/2);
+              
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery-5,centerx-5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery-5,centerx,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery,centerx+5,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery-5,centerx+10,centery-5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			
+
+			  this.paneo.svgx = this.paneo.container.line(centerx-10,centery,centerx-5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+			  
+			  this.paneo.svgx = this.paneo.container.line(centerx-5,centery,centerx,centery+5);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx,centery+5,centerx+5,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+			  this.paneo.svgx = this.paneo.container.line(centerx+5,centery,centerx+10,centery);
+		      this.paneo.svgx.attr({fill: ssBase.color["SG"].htmlcolor,"stroke": "#000", "stroke-width": 1});
+
+
+            } 
+
+
+			this.isReady = true;
+		    if(vtype === "" || vtype === "S" || hasScreenOnly){
+		      if(ismosm != "I"){	
+			    this.screen = new ssScreen(this,this.paneo.x,this.paneo.y).render();			
+		      }  
+		    }  
+			// use to put a bounding box when its selected  --- for hliting
+			this.x1 = this.paneo.x;
+			this.y1 = this.paneo.y;
+			this.x2 = this.paneo.x + this.paneo.w; 
+			this.y2 = this.paneo.y + this.paneo.h;
+			this.instanceId = data.model.InstanceId;
+			this.client.hliteObject(this);
+	
+		
+			return this;
+		};
+	};
+
+});
---
+++ libs/ssgraph/wallsystem/ssDoorOpening.js
@@ -0,0 +1,189 @@
+sap.ui.define([
+	"zss5lcabase/common/ssMath",
+	"zss5lcabase/common/coreFunctions",
+	"zss5lgraphics/libs/ssgraph/common/ssBase"
+], function(ssMath, core, ssBase) {
+	return function(client, data, myParent, x, y) {
+		this.data = data;
+		this.client = client;
+		this.x = x;
+		this.y = y;
+		this.ssparent = myParent;
+		this.container = undefined;
+		this.openings = {
+			container: undefined,
+			svg: undefined,
+			x: x,
+			y: y
+		};
+		this.mods = [];
+		this.isReady = false;
+		this.w = 0;
+		this.h = 0;
+
+		if (this.x === undefined) {
+			this.x = 0;
+		}
+		if (this.y === undefined) {
+			this.y = 0;
+		};
+
+		this.setData = function(data) {
+			this.data = data;
+		};
+		this.getData = function() {
+			return this.data;
+		};
+		this.setParent = function(parent) {
+			this.ssparent = parent;
+		};
+		this.getParent = function() {
+			return this.ssparent;
+		};
+		this.getClient = function() {
+			return this.client;
+		};
+
+		this.calculate = function() {
+			var data = this.getData();
+		};
+
+		this.render = function() {
+			var w = 0;
+			var h = 0;
+			var tw = 0;
+			var px = this.x;
+			var py = this.y;
+			var color = "WH";
+
+			this.container = this.getParent().nested();
+			var data = this.getData();
+			this.isReady = true;
+
+			if (data.model["WIDTH"] != undefined) {
+				w = Number(data.model["WIDTH"].value);
+				if (w == undefined) {
+					w = 1;
+				}
+			}
+			if (data.model["HEIGHT"] != undefined) {
+				h = Number(data.model["HEIGHT"].value);
+				if (h == undefined) {
+					h = 1;
+				}
+			}
+
+			if (data.model["FRAME.COLOR"] != undefined) {
+				color = data.model['FRAME.COLOR'].value;
+			};
+
+			h = this.client.convert(h);
+			w = this.client.convert(w);
+			this.w = w;
+			this.h = h;
+			this.color = color;
+
+			if (w <= 0) return;
+			if (h <= 0) return;
+
+			// add in mod openings
+			this.renderOpenings();
+
+			this.isReady = true;
+			// use to put a bounding box when its selected  --- for hliting
+			this.x1 = this.openings.x1;
+			this.y1 = this.openings.y1;
+			this.x2 = this.openings.x2;
+			this.y2 = this.openings.y2;
+			this.instanceId = data.model.InstanceId;
+			this.client.hliteObject(this);
+			return this;
+
+		};
+		this.renderOpenings = function() {
+			var w;
+			var h;
+			var tw = 0;
+			var color = this.color;
+			var drop = 0;
+			var rise = 0;
+
+			if (data.model["WIDTH"] != undefined) {
+				w = Number(data.model["WIDTH"].value);
+				if (w == undefined) {
+					w = 1;
+				}
+			}
+			if (data.model["HEIGHT"] != undefined) {
+				h = Number(data.model["HEIGHT"].value);
+				if (h == undefined) {
+					h = 1;
+				}
+			}
+			if (data.model["EXTERIOR.COLOR"] != undefined) {
+				color = data.model['EXTERIOR.COLOR'].value;
+			};
+			if (color === undefined) {
+				color = "WH";
+			}
+
+			w = this.client.convert(w);
+			h = this.client.convert(h);
+			tw = this.client.convert(tw);
+
+			x = this.x;
+			y = this.y;
+
+			this.openings.container = this.container.nested();
+			this.openings.w = w;
+			this.openings.drop = drop;
+			this.openings.rise = rise;
+			this.openings.h = h;
+			this.openings.x1 = x;
+			this.openings.y1 = y;
+			this.openings.x2 = this.openings.x1 + w;
+			this.openings.y2 = this.openings.y1 + h;
+			this.openings.y3 = this.openings.y1;
+
+			this.openings.svg = this.openings.container.polygon([this.openings.x1, this.openings.y1,
+				this.openings.x1, this.openings.y2,
+				this.openings.x2, this.openings.y2,
+				this.openings.x2, this.openings.y3
+			]);
+			this.openings.svg.attr({
+				fill: ssBase.color[color].htmlcolor,
+				"stroke": "#000",
+				"stroke-width": 1
+			});
+			//			this.headerextrusion.svg.front();
+
+			var objmods = core.getAllModels(this.getData());
+			if (objmods === undefined) {
+				return;
+			}
+
+			var x = this.openings.x1;
+			var y = this.openings.y1;
+			for (var j = 0; j < objmods.length; j++) {
+				var amod = objmods[j];
+				if (amod === undefined) {
+					continue;
+				}
+				if (amod.model["HEIGHT"] != undefined) {
+					h = Number(amod.model["HEIGHT"].value);
+					if (h == undefined) {
+						h = 1;
+					}
+				}
+				h = this.client.convert(h);
+				var mod = this.getClient().draw(amod, x, y, this.openings.container);
+				this.mods.push(mod);
+				try {
+					x = mod.x1;
+					y = mod.y2;
+				} catch (e) {}
+			}
+		};
+
+	};
+});
